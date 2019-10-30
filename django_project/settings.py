edef004644d693a5ed172f10017e3cbd1c7cf0ba
David Freitag <freitagdavid@outlook.com>
2019-10-29 20:49:52 -0400

    django_project/settings: Punctuation fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DUBUG_VALUE']
+DEBUG = os.environ('DUBUG_VALUE')
 
 ALLOWED_HOSTS = ['dfreitag-django-blog.herokuapp.com']
 
